2ae2786035300202b6633c0fa73ef16b700da1de
Gabriel Cândido Có <you@example.com>
2021-11-23 20:52:14 -0300

    primeiro commit

---
+++ JogoAdvinhacao/jogo.js
@@ -0,0 +1,65 @@
+const Colors = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 
+'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 
+'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 
+'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 
+'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 
+'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 
+'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 
+'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 
+'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 
+'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 
+'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 
+'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen']
+var SelectedColors = [];
+var Count = 10;
+var colorMain = "";
+
+window.onload = function (event) {
+    setSelectedColor();
+    startGame();
+}
+
+function startGame() {
+    let correct = false;
+
+    while (!correct && Count != 0) {
+        let answer = prompt(`Qual é a cor ? \nVidas: ${Count}  \nCores: ${this.SelectedColors.join(', ')}`);
+        correct = checkAnswer(answer.toLowerCase());
+        console.log(colorMain);
+
+
+        if (!correct) {
+            Count--;
+            alert(`Incorreto. Tente novamente`)
+        }
+    }
+
+
+    if (Count == 0)
+        alert("Infelizmente você perdeu! Pressione F5 para recomeçar.");
+    else {
+        alert("Parabéns, você ");
+        document.getElementsByTagName("body")[0].style.backgroundColor = colorMain;
+    }
+}
+
+function setSelectedColor() {
+    this.SelectedColors = [];
+    for (let i = 0; i < 10; i++) {
+        let index = Math.floor(Math.random() * Colors.length);
+        this.SelectedColors.push(Colors[index]);
+    }
+
+    this.colorMain = this.SelectedColors[Math.floor(Math.random() * this.SelectedColors.length)].toLowerCase();
+}
+
+function resetGame() {
+    Count = 5;
+    setSelectedColor();
+    startGame();
+}
+
+function checkAnswer(text) {
+    return colorMain == text;
+}
+
